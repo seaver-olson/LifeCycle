396e7fd596f951e3beced2659a9b7a093513dca2
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2025-06-02 15:39:39 -0500

    Create game.h

---
+++ include/game.h
@@ -0,0 +1,48 @@
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+typedef enum{
+    TILE_EMPTY,
+    TILE_GRASS,
+    TILE_WATER,
+    TILE_FOREST,
+    TILE_MOUNTAIN,
+    TILE_ROAD,
+    TILE_BUILDING,
+} TileType;
+
+typedef struct {
+    TileType type;
+} Tile;
+
+typedef struct {
+    float x;         
+    float y;
+    float zoom;      // e.g., 1.0 = 100%, 2.0 = zoomed in
+} Camera;
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define ZOOM_MIN 0.25f
+#define ZOOM_MAX 4.0f
+
+#define WINDOW_TITLE "LifeCycle Simulator"
+#define BASE_TILE_SIZE 20
+
+#define GRID_COLS 200
+#define GRID_ROWS 200
+
+extern Tile tileMap[GRID_ROWS][GRID_COLS];
+extern Camera camera;
+extern int tile_size;
+extern float *noiseMap;
+
+bool init(SDL_Window **window, SDL_Renderer **renderer);
+void game_loop(SDL_Renderer *renderer);
+void cleanup(SDL_Window *window, SDL_Renderer *renderer);
+void render_grid(SDL_Renderer *renderer);
+void place_tile(int x, int y, TileType type);
+
+float *createFastNoiseMap(int width, int height);
+void freeFastNoiseMap(float *map);
