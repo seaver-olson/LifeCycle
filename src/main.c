4d4209458c70de1c711ba337307d4da1c39a060f
seaver-olson <sjolson007@gmail.com>
2025-06-10 00:12:35 -0500

    found bug in loading sequence causing map to change everytime it is loaded and saved, most likely a error in the nested loops, will fix when it isnt midnight

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "mapFile.h"
 
 int saveFlag = 0;
+int loadFlag = 0;
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
@@ -10,29 +11,31 @@ int main(int argc, char *argv[]) {
     if (!init(&window, &renderer)) {
         return 1;
     }
-    if (argc == 1) printf("no args; running normally\n");
-    else if (argc == 2) {
-        if (strcmp(argv[1], "0") == 0) {
-            printf("Running without save mode\n");
-            generate_tile_map();
-        } else if (strcmp(argv[1], "1") == 0) {
+    if (argc == 1) {
+        printf("no args; running normally\n");
+        generate_tile_map();
+    } else if (argc >= 2) {
+        if (strcmp(argv[1], "-s") == 0) {
             printf("Running in save mode\n");
             saveFlag = 1;
-            generate_tile_map();
-        } else if (strcmp(argv[1], "2") == 0) {
-            printf("Error: to load map please follow first arg with map file\n");
-            return 1;
         } else {
             printf("Error: unrecognized arg\n");
             return 1;
         }
-    } else if (argc == 3 && strcmp(argv[1], "2") == 0){
-        printf("Attempting to load map from %s\n", argv[2]);
-        loadMap(argv[2], &tileMap[0][0]);
-    } else {
-        printf("invalid usage of arguments");
-        return 1;        
+    } if (argc >= 4){
+        if (strcmp(argv[2], "-l") == 0 || strcmp(argv[1], "-l") == 0){
+            if (argc == 3) loadFlag = 2; //reusing loadFlag lol
+            else loadFlag = 3;
+            if (strstr(argv[loadFlag], ".map") != NULL){
+                printf("Attempting to load map from %s\n", argv[loadFlag]);
+                loadMap(argv[loadFlag], &tileMap[0][0]);
+            } else {
+                printf("Error: Please include a .map file to continue");
+                return 1;
+            }
+        }
     }
+    if (loadFlag == 0) generate_tile_map();
     game_loop(renderer, saveFlag);
     cleanup(window, renderer);
     return 0;
